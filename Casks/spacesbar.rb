d3e35f0a96310799a46c3a377cd8699a59a90f80
Albert G <516972+alber70g@users.noreply.github.com>
2026-04-22 18:19:00 +0200

    fix(cask): drop 'macOS' from desc (brew style: Cask/Desc)

--- Casks/spacesbar.rb
+++ Casks/spacesbar.rb
@@ -4,7 +4,7 @@ cask "spacesbar" do
 
   url "https://github.com/alber70g/SpacesBar/releases/download/v#{version}/SpacesBar-#{version}-arm64.zip"
   name "SpacesBar"
-  desc "Menu bar app showing apps per macOS space"
+  desc "Menu bar app showing apps per space"
   homepage "https://github.com/alber70g/SpacesBar"
 
   depends_on macos: ">= :tahoe"
